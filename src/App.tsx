2ed870c8d215818df0ee56116365dfc2fcd5a890
Eduardo Morales <emoral435@gmail.com>
2023-03-06 17:40:06 -0600

    Base files added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='font-bold underline'>This is vite, with react and tailwind as the frontend and firebase as the backend. Redux as a nifty library and framer motion to hopefully make this look pretty</h1>
+    </div>
+  )
+}
+
+export default App
