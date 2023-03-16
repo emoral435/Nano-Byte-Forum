fc5a4e17dc0ae23c7bf57a0fe29112b4f85759fb
Eduardo Morales <emoral435@gmail.com>
2023-03-15 22:06:40 -0500

    Final Layer needed for startup page

--- src/components/Startup/Startup.tsx
+++ src/components/Startup/Startup.tsx
@@ -1,26 +1,44 @@
 import { useRef } from 'react'
 import { motion } from 'framer-motion'
-import NanoImg from '/src/assets/Nano_logo.png'
+import NanoImg from '/src/assets/Nano-logo-dark.svg'
 import OTP from '/src/assets/OTP.jpg'
-import BasicButton from '../common/Button'
 import StartupNav from './StartupNav'
+import Statistics from './Statistics'
 
 const LayerOne = () => {
     return (
         <>
-            <h1 className='relative text-7xl font-bold flex flex-col sm:flex-row justify-between items-center select-none text-[#4384d0] h-1/4 sm:h-[100vh] w-full bg-stone-300'>
-                <div className='flex gap-4 m-4'>
-                    <div>Nano</div>
-                    <motion.div className={'w-20 h-16'} whileHover={{scale: 1.2}}>
-                        <img src={NanoImg} alt="Nano Byte Logo" className='w-full h-full'/>
-                    </motion.div>
-                    <div>Byte</div>
+            <h1 className='text-7xl font-["Georgia"] flex flex-col-reverse items-center justify-center md:flex-row md:justify-around md:items-center select-none text-[#333333] h-[40vh] w-full bg-stone-300'>
+                <div className='flex gap-12 justify-center items-start flex-col m-4'>
+                    <div className="flex justify-center">
+                        <div>Nano</div>
+                        <motion.div className={'w-20 h-16 relative'} whileHover={{scale: 1.2}}>
+                            <img src={NanoImg} alt="Nano Byte Logo" className='w-full h-full absolute top-2'/>
+                        </motion.div>
+                        <div>Byte</div>
+                    </div>
+                    <div className='flex justify-start flex-row md:flex-col gap-2 md:gap-0'>
+                        <p className='hidden md:flex text-3xl text-[#5c5c5c]'>Join the worlds fastest</p>
+                        <p className='hidden md:flex text-3xl text-[#5c5c5c]'>growing community.</p>
+                        <p className='md:hidden text-3xl text-[#5c5c5c]'>Join the worlds fastest growing community.</p>
+                    </div>
+                </div>
+                <div className='flex justify-center overflow-hidden'>
+                    <img src={OTP} alt="Woman On Phone" className='hidden md:block object-cover md:h-[50rem] md:mt-64 hover:scale-100 scale-[120%] transition ease-in-out duration-200'/>
                 </div>
             </h1>
             <div className='flex flex-col items-center'>
-                <h2 className='text-3xl'>Join the worlds fastest growing community.</h2>
-                <h2 className='text-3xl'>With channels varying from cooking all the way to hiking, there is something for you here.</h2>
             </div>
+        </>
+    )
+}
+
+const LayerTwo = () => {
+    return (
+        <>
+            <h2 className='w-full flex flex-col justify-start items-start'>
+                <Statistics />
+            </h2>
         </>
     )
 }
@@ -32,6 +50,7 @@ const Startup = () => {
     <div className='font-[Nunito] relative'>
         <StartupNav />
         <LayerOne />
+        <LayerTwo />
     </div>
   )
 }
---
+++ src/components/Startup/Statistics.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { PhoneAndroid } from '@mui/icons-material'
+import androidPhone from '/src/assets/androidPhone.svg'
+import cloud from '/src/assets/cloud.svg'
+import personReading from '/src/assets/personReading.svg'
+import personExplaining from '/src/assets/personExplaining.svg'
+
+const stats = [
+    {url: cloud, smallInfo: "Connects to the cloud, saving your profile and messages.", bigNumber: "10M", bigMsg: "gigabytes of data stored securely", id :"cloud"},
+    {url: androidPhone, smallInfo: "Use any device, on the go and on the fly.", bigNumber: "IOS & ANDROID", bigMsg: "devices supported", id: "androidPhone"},
+    {url: personReading, smallInfo: "Look at previous questions to see if your question has already been asked.", bigNumber: "67M", bigMsg: "posts available", id: "personReading"},
+    {url: personExplaining, smallInfo: "Interact with millions of users to grow your reach.", bigNumber: "17M", bigMsg: "users online",id: "personStats"}
+]
+
+interface Props {
+    imageUrl : string,
+    smallInfo: string,
+    bigNumber: string
+    bigMsg: string
+}
+
+const Stat = ({ imageUrl, smallInfo, bigNumber, bigMsg } : Props) => (
+    <div className='flex flex-col items-center text-xl text-[#5c5c5c] gap-4'>
+        <div className='bg-[#faf1e2] flex justify-center w-full gap-4 p-4'>
+            <h3 className='text-[#38434f] flex flex-col justify-center'>
+                <div className='flex justify-center'>
+                    <h4 className='text-5xl font-bold'>{bigNumber}</h4>
+                </div>
+                <div className='text-2xl'>{bigMsg}</div>
+            </h3>
+            <img src={imageUrl} alt="Accompanying Statistic Image" className='h-32 w-32'/>
+        </div>
+        <h2>{smallInfo}</h2>
+    </div>
+)
+
+const Statistics = () => {
+  return (
+    <div className='m-12 md:w-[40%] font-["Georgia"]'>
+        <h3 className='text-[#333333] text-3xl mb-12'>Establish connections that will last a lifetime.</h3>
+        <div className='grid grid-auto-fit gap-8'>
+            {stats.map(({url, smallInfo, bigNumber, bigMsg, id}) => (
+                <div key={id}>
+                    <Stat imageUrl={url} smallInfo={smallInfo} bigNumber={bigNumber} bigMsg={bigMsg} />
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Statistics
