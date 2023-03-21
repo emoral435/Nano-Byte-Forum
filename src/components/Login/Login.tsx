ae16445a3f3c52340bb5ecc67c608d9671c46ce9
Eduardo Morales <emoral435@gmail.com>
2023-03-21 18:43:47 -0500

    Abstracted login component to reuse for sign up component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import Form from '../common/Form'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { signInWithEmailAndPassword, getAuth } from 'firebase/auth'
 import { app } from '../../firebase/firebase-config'
-import { LayerFour } from '../Startup/LayerFour'
 import user from '/src/assets/loginUser.svg'
 import bg from '/src/assets/loginBackground.svg'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AuthorizingPages from '../common/AuthorizingPages'
 
 const Login = () => {
     const [email, setEmail] = useState('')
@@ -46,23 +45,7 @@ const Login = () => {
 
   return (
     <div className='w-full flex md:flex-row flex-col-reverse items-center '>
-      <div className='w-[35rem] xl:flex hidden bg-[#3f3d56] xl:h-[100vh] items-center' >
-        <img src={bg} alt="login background" className=''/>
-      </div>
-      <div className='w-full h-[100vh] bg-[#ebf3f9] flex flex-col justify-center items-center lg:items-start'>
-        <div className='flex flex-col shadow-xl sm:p-12 py-12 lg:ml-10 xl:ml-20 bg-white gap-20 md:scale-100 scale-75'>
-          <div className='flex md:flex-row flex-col-reverse gap-8'>
-            <div>
-              <h1 className='text-7xl text-[#333333] mb-6' >Welcome Back.</h1>
-              <Form title='Login' sideTitle='Sign Up' setEmail={setEmail} setPassword={setPassword} handleForm={login} handleSide={signUp}/>
-            </div>
-            <img src={user} alt="login user" className='md:h-full h-[10rem]' />
-          </div>
-          <div className='flex justify-center'>
-            <LayerFour />
-          </div>
-        </div>
-      </div>
+      <AuthorizingPages msg={'Welcome Back.'} bg={bg} user={user} setEmail={setEmail} setPassword={setPassword} handleFormSubmit={login} handleSide={signUp}/>
       <ToastContainer />
     </div>
   )
---
+++ src/components/common/AuthorizingPages.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Form from './Form'
+import { LayerFour } from '../Startup/LayerFour'
+
+interface Props {
+    bg: string,
+    msg: string,
+    setEmail: (email: string) => void ,
+    setPassword: (password: string) => void,
+    handleFormSubmit(): void,
+    handleSide(): void,
+    user: string
+
+}
+
+const AuthorizingPages = ({bg, msg, setEmail, setPassword, handleFormSubmit, handleSide, user} :Props) => {
+  return (
+    <div className='flex md:flex-row flex-col-reverse items-center'>
+        <div className='w-[35rem] xl:flex hidden bg-[#3f3d56] xl:h-[100vh] items-center' >
+            <img src={bg} alt="login background" className=''/>
+        </div>
+        <div className='w-full h-[100vh] bg-[#ebf3f9] flex flex-col justify-center items-center lg:items-start'>
+            <div className='flex flex-col shadow-xl sm:p-12 py-12 lg:ml-10 xl:ml-20 bg-white gap-20 md:scale-100 scale-75'>
+            <div className='flex md:flex-row flex-col-reverse gap-8'>
+                <div>
+                <h1 className='text-7xl text-[#333333] mb-6' >{msg}</h1>
+                <Form title='Login' sideTitle='Sign Up' setEmail={setEmail} setPassword={setPassword} handleForm={handleFormSubmit} handleSide={handleSide}/>
+                </div>
+                <img src={user} alt="login user" className='md:h-full h-[10rem]' />
+            </div>
+            <div className='flex justify-center'>
+                <LayerFour />
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AuthorizingPages
