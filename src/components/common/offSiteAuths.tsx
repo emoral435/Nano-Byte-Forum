486eed434d9b01f411fb36af8712af42bd6c5912
Eduardo Morales <emoral435@gmail.com>
2023-03-21 17:56:09 -0500

    Google and github sign-up added, working on refactoring the login component

--- src/components/common/offSiteAuths.tsx
+++ src/components/common/offSiteAuths.tsx
@@ -1,18 +1,56 @@
 import BasicButton from "./Button"
 import GoogleIcon from '@mui/icons-material/Google';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithRedirect, getRedirectResult, GoogleAuthProvider, GithubAuthProvider, User } from 'firebase/auth';
+import Button from '@mui/material/Button'
+import { useNavigate } from "react-router-dom";
+import { app } from "../../firebase/firebase-config";
+
 
 
 const OffSiteAuths = () => {
+    const navigate = useNavigate()
+    
+    const success = (type: string) => {
+        sessionStorage.setItem('login token', type + ' token')
+        console.log("success")
+        navigate('/home')
+    }
+    
+    const handleGoogleAuth = () => {
+        handleAuth('google')
+    }
+    
+    const handleGithubAuth = () => {
+        handleAuth('github')
+    }
+
+    const handleAuth = (type: string) => {
+        console.log(type)
+        const provider = (type == 'github') ? new GithubAuthProvider : new GoogleAuthProvider
+        const auth = getAuth();
+        signInWithRedirect(auth, provider)
+        getRedirectResult(auth)
+            .then((result: any) => {
+                console.log(result)
+                success(type)
+            }).catch((err) => { 
+                console.log(err.code)
+            })
+    }
+
   return (
-    <div className="w-full text-[#5c5c5c]">
+    <div className="w-full text-[#5c5c5c] flex flex-col gap-4">
         <div className="w-full flex justify-center items-center" id="continue-with">
             <span className="mx-4">Or continue with</span>
         </div>
         <div className="w-full flex justify-around">
-            <GoogleIcon />
-            <GitHubIcon />
+            <Button color='primary' size='large' variant='contained' className="whitespace-nowrap" sx={{paddingLeft: 8, paddingRight: 8}} onClick={handleGoogleAuth} >
+                <GoogleIcon />
+            </ Button>
+            <Button color='primary' size='large' variant='contained' className="whitespace-nowrap" sx={{paddingLeft: 8, paddingRight: 8}} onClick={handleGithubAuth}>
+                <GitHubIcon />
+            </ Button>
         </div>
     </div>
   )
